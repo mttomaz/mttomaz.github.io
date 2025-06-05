a64ed01562da34349e34ed9606d3022d0b205518
mttomaz <mttomaz@duck.com>
2025-06-05 08:48:01 -0300

    feat: add projects

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-const cards = document.querySelector('.cards')
-const repos = 'https://api.github.com/users/mttomaz/repos';
+const cards = document.querySelector(".cards");
+const repos = "https://api.github.com/users/mttomaz/repos";
 
 fetch(repos)
-  .then(res => res.json())
-  .then(data => {
-    data.map((d, i) => {
-      if ( d.stargazers_count === 0 ) {
-      return;
-      }
-      cards.innerHTML += `
-        <a href="${d.html_url}"><article>
-          <h3>${d.name}</h3>
-          <p>${d.description}</p>
-          <p><span class="material-symbols-rounded">star</span> ${d.stargazers_count}</p>
+  .then((res) => res.json())
+  .then((data) =>
+    data
+      .filter((repo) => repo.stargazers_count > 0)
+      .sort((a, b) => b.stargazers_count - a.stargazers_count)
+      .map((repo) => {
+        cards.innerHTML += `
+        <a href="${repo.html_url}"><article>
+          <h3>${repo.name}</h3>
+          <p>${repo.description}</p>
+          <p><span class="material-symbols-rounded">star</span> ${repo.stargazers_count}</p>
         </article></a>
       `;
-    })
-  })
+      }),
+  );
 
 const age = document.querySelector(".age");
-age.textContent = `${Math.floor((new Date()-new Date("2004-04-06"))/31557600000)}`
+age.textContent = `${Math.floor((new Date() - new Date("2004-04-06")) / 31557600000)}`;
